f2fd5236794bed9bdc63eb9f94bb34c97036f41a
Luke H <purelyfortoontown2@gmail.com>
2023-10-23 14:16:39 +0100

    First commit

---
+++ src/main/java/com/lbg/SearchableTree.java
@@ -0,0 +1,32 @@
+package com.lbg;
+import java.util.ArrayList;
+public class SearchableTree
+{
+    private ArrayList<String> items = new ArrayList<>();
+    public String populateContainer()
+    {
+        String str= new String("selvyn");
+        items.add(str);
+        items.add("trains");
+        items.add("lecture");
+        items.add("graduate");
+        items.add("company");
+        items.add("taxes");
+        return str;
+    }
+    public int find( String key )
+    {
+        return items.indexOf(key);
+    }
+    public boolean findIfLastItem( String key )
+    {
+        boolean result = false;
+        int length = items.size();
+        return length-1 == items.indexOf(key);
+    }
+    public boolean findIfFirstItem( String key )
+    {
+        boolean result = false;
+        return 0 == items.indexOf(key);
+    }
+}
---
+++ src/test/java/com/lbg/SearchableTreeTest.java
@@ -0,0 +1,24 @@
+package com.lbg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchableTreeTest {
+
+    @Test
+    void populateContainer() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void findIfLastItem() {
+    }
+
+    @Test
+    void findIfFirstItem() {
+    }
+}
